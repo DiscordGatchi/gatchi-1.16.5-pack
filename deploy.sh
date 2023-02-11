580e10dc5972aa15a4beb7fdd44fb6acd4cf8fde
Synqat <ken@slinky.link>
2023-02-11 13:18:39 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 cd /home/minecraft/repository || exit 1
 
-screen -S "mc-server" -X stuff "say Deploying new version...$(printf \\r)"
-screen -S "mc-server" -X stuff "save-all$(printf \\r)"
-screen -S "mc-server" -X stuff "stop$(printf \\r)"
-
 git pull
 
-screen -S "mc-server" -X stuff "start$(printf \\r)"
-screen -S "mc-server" -X stuff "say Deployed new version!$(printf \\r)"
+echo "\"done\""
