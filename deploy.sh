99926627594c6a9c99b196639d3ee4ccc07ec8df
Synqat <ken@slinky.link>
2023-02-11 17:49:04 -0700

    fix: attempt to fix action for testing

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-cd /home/minecraft/repository || exit 1
+cd /home/minecraft/server || exit 1
 
-docker compose down
+docker container stop minecraft
+docker container rm minecraft
 git pull
-docker compose up -d
+docker run -d -v \
+  /home/minecraft/server:/data \
+  -e TYPE=FABRIC \
+  -e VERSION=1.18.2 \
+  -e MEMORY=8G \
+  -e EULA=TRUE \
+  -p 25565:25565 \
+  -e "PACKWIZ_URL=https://discordgatchi.github.io/the-gatchi-pack/pack.toml" itzg/minecraft-server
