a5a23b3d0144bf28a444da3188f23e7698633b41
Synqat <ken@slinky.link>
2023-02-13 17:37:28 -0700

    feat: 1.0.0
    BREAKING CHANGE: not really i just want version 1.0.0

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ docker run -d -v \
   /home/minecraft/server:/data \
   -e TYPE=FABRIC \
   -e VERSION=1.18.2 \
-  -e MEMORY=6G \
+  -e MEMORY=14G \
   -e EULA=TRUE \
   -p 25565:25565 \
+  -p 24454:24454/udp \
   --name minecraft \
   -e "PACKWIZ_URL=https://discordgatchi.github.io/the-gatchi-pack/pack.toml" itzg/minecraft-server
